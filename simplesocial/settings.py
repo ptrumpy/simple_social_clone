99684f3d11859d044a41f4566d9ccc4bb7c3fa9b
ptrumpy <40766989+ptrumpy@users.noreply.github.com>
2022-03-15 20:30:57 -0500

    ready to make accounts migrations

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = Path(BASE_DIR)/'static'
 
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
